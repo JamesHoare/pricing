c0e77ad9ed86ca449524879c2c49c61e34e605c2
j.hoare <jhoare0@gmail.com>
2014-04-15 15:44:52 +0100

    Adding controller tests

--- src/main/java/com/netaporter/pricing/domain/Product.java
+++ src/main/java/com/netaporter/pricing/domain/Product.java
@@ -92,4 +92,7 @@ public class Product implements ProductPricing {
     }
 
 
+
+
+
 }
---
+++ src/test/java/ProductPricingControllerTest.java
@@ -0,0 +1,55 @@
+import com.netaporter.pricing.PricingApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.stream.Stream;
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+
+/**
+ * Created by j.hoare on 4/15/14.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles("test")
+@SpringApplicationConfiguration(classes = PricingApplication.class)
+@WebAppConfiguration
+public class ProductPricingControllerTest {
+
+
+        @Autowired
+        private WebApplicationContext applicationContext;
+
+        @Test
+        public void testRoutes() {
+            final MockMvc mockMvc = MockMvcBuilders
+                    .webAppContextSetup(applicationContext)
+                    .build();
+
+            Stream.of("/products").forEach(path -> {
+                try {
+                    mockMvc
+                            .perform(get(path))
+                            .andExpect(status().isOk())
+                            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
+
+
+
