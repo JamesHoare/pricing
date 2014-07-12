fe9d9ec63417e1b31f29ac3878d516c2cf39526f
jameshoare <jhoare0@gmail.com>
2014-07-12 15:27:09 +0100

    polishing

--- src/main/java/com/netaporter/pricing/api/ProductPricingController.java
+++ src/main/java/com/netaporter/pricing/api/ProductPricingController.java
@@ -1,14 +1,14 @@
 package com.netaporter.pricing.api;
 
 
-import com.netaporter.pricing.domain.Product;
-import com.netaporter.pricing.domain.ProductPricingRepository;
+import com.netaporter.pricing.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 
 /**
@@ -27,8 +27,8 @@ public class ProductPricingController {
 
 
     @RequestMapping(value = "/products", method = RequestMethod.GET)
-    public List<Product> getProducts() {
-        return pricingRepository.findAll();
+    public Optional<List<Product>> getProducts() {
+            return pricingRepository.getProducts();
     }
 
 
--- src/main/java/com/netaporter/pricing/domain/ProductPricingRepository.java
+++ src/main/java/com/netaporter/pricing/domain/ProductPricingRepository.java
@@ -1,15 +1,18 @@
 package com.netaporter.pricing.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.Repository;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by jameshoare on 13/04/2014.
  */
 @Validated
-public interface ProductPricingRepository extends JpaRepository<Product,Integer> {
+public interface ProductPricingRepository extends Repository<Product,Integer> {
 
-
+    Optional<List<Product>> getProducts();
 
 
 
