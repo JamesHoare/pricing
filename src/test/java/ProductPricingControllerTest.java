835935e4a6dce22c9cc6441407b935158a611028
jameshoare <jhoare0@gmail.com>
2014-07-12 10:28:11 +0100

    Upgrades

--- src/test/java/ProductPricingControllerTest.java
+++ src/test/java/ProductPricingControllerTest.java
@@ -1,4 +1,4 @@
-import com.netaporter.pricing.PricingApplication;
+import com.netaporter.pricing.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
